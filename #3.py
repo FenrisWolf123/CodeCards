dd1af608c6867c0e7450db0920a7c56f67fe479a
FenrisWolf123 <bhargav.kulkarni1@gmail.com>
2018-03-23 22:09:14 +0530

    Add files via upload

---
+++ #3.py
@@ -0,0 +1,10 @@
+"""
+P(n) is an iterative sequence as defined as below:
+    if n is even -> n/2
+    if n is odd -> 3n+1
+    if n is 1 stop
+
+P(13) => 13->40->20->10->5->16->8->4->2->1
+
+Which number under a million makes the largest sequence
+"""
---
+++ #4.py
@@ -0,0 +1,7 @@
+"""
+Factorial of n is the product of the first n natural numbers.
+Primorial of n is the product of the first n prime numbers.
+
+The nth euclid number is the primorial of n + 1.
+
+Which is the 15th(10th) euclid number
